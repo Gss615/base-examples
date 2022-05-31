e8095c01f98e8c66af055a06850915fba3391819
Gss615 <gao.yh1991@gmail.com>
2022-05-31 16:34:51 +0800

    Create auth.guard.ts

---
+++ base-angular/src/app/@core/auth.guard.ts
@@ -0,0 +1,4 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth/auth.service';
