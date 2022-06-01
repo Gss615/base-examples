0e88676f971e4ceba18b70d15081db172a0f08a0
home <gao.yh1991@gmail.com>
2022-06-01 16:21:45 +0800

    feat: ngzorro&sharedModule

--- base-angular/src/app/@shared/shared.module.ts
+++ base-angular/src/app/@shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SHARED_ZORRO_MODULES } from './shared-zorro.module';
+import { SHARED_ZORRO_MODULES } from './modules/ng-zorro-antd/shared-zorro.module';
 
 // TODO  注册是什么，和引入有什么区别
 const MODULES = [
