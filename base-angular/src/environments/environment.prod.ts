e38eefd78a5c63cbd6259f544271b7fdad2ecb9b
home <gao.yh1991@gmail.com>
2022-06-02 15:45:48 +0800

    style

--- base-angular/src/environments/environment.prod.ts
+++ base-angular/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
   production: true
 };
+
+export const PROJECT_NAME = packageInfo.name;
--- base-angular/src/environments/environment.ts
+++ base-angular/src/environments/environment.ts
@@ -1,20 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import packageInfo from '../../package.json';
 
 export const environment = {
   production: false,
-  PROJECT_NAME:'ng'
 };
 
 
-export const PROJECT_NAME = 'ng'
+export const PROJECT_NAME = packageInfo.name || 'ng'
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+//  debug Zone.run 错误，只在开发环境引入，避免产生性能问题
+import 'zone.js/plugins/zone-error'; 
